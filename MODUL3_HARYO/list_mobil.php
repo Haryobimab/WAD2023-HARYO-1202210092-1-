e6cc4e693ad0e6b948b4093588bf3a7d16a28c92
Haryobimab <91707459+Haryobimab@users.noreply.github.com>
2023-11-13 20:20:34 +0700

    Update list_mobil.php

--- MODUL3_HARYO/list_mobil.php
+++ MODUL3_HARYO/list_mobil.php
@@ -10,30 +10,25 @@
     <center>
         <div class="container">
             <h1>List Mobil</h1>
-
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope ="col">#</th>
+                        <th scope ="col">Nama Mobil</th>
+                        <th scope ="col">Brand Mobil</th>
+                        <th scope ="col">Warna Mobil</th>
+                        <th scope ="col">Tipe Mobil</th>
+                        <th scope ="col">Harga</th>
+                        <th scope ="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
             <?php
             include("connect.php");
 
             // Buatlah query untuk mengambil data dari database (gunakan query SELECT)
-            $query = "SELECT * FROM showroom_mobil ";
-            $result = mysqli_query($connect, $query);
-            if ($result) {
-                while ($row = mysqli_fetch_assoc($result)) {
-                    $nama_mobil = $row['nama_mobil'];
-                    $brand_mobil  = $row['brand_mobil'];
-                    $warna_mobil  = $row['warna_mobil'];
-                    $tipe_mobil  = $row['tipe_mobil'];
-                    $harga_mobil  = $row['harga_mobil'];
-                    echo '<tr>
-                    <th scope="row">' . $brand_mobil . '</th>
-                    <td>' . $nama_mobil . '</td>               
-                    <td>' . $brand_mobil . '</td>
-                    <td>' . $warna_mobil . '</td>
-                    <td>' . $tipe_mobil . '</td>
-                    <td>' . $harga_mobil . '</td>
-                    </tr>';
-                }
-            }
+            $query = mysqli_query($connect, "SELECT * FROM showroom_mobil");
+    
 
             
 
@@ -47,19 +42,30 @@
             // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel'
 
             //<!--  **********************  1  **************************     -->
-
-            
-            
-
-
-
-
-
-
+            if($query){
+                while ($selects = mysqli_fetch_assoc($query)){
+            ?>
+            <tr>
+                <th scope ="row"><?= $selects['id'] ?></th>
+                <td><?= $selects['nama_mobil'] ?></td>
+                <td><?= $selects['brand_mobil'] ?></td>
+                <td><?= $selects['warna_mobil'] ?></td>
+                <td><?= $selects['tipe_mobil'] ?></td>
+                <td><?= $selects['harga_mobil'] ?></td>
+                <td><a href="form_detail_mobil.php?id=<?= $selects['id'] ?>" class="btn btn-primary">Detail</td>
+            </tr>
+            <?php
+                }
+            }
+        
 
             //<!--  **********************  1  **************************     -->
 
             //<!--  **********************  2  **************************     -->
+
+                    else {
+                echo"Tidak ada data di database";
+            }
 
             
             
