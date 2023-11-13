d39b22ce6a370432c37ab4d8b85779d531feb0f1
Haryobimab <91707459+Haryobimab@users.noreply.github.com>
2023-11-13 20:29:45 +0700

    Update form_detail_mobil.php

--- MODUL3_HARYO/form_detail_mobil.php
+++ MODUL3_HARYO/form_detail_mobil.php
@@ -12,7 +12,8 @@
             $id = $_GET['id'];
             // Buatlah query untuk mengambil masing-masing data berdasarkan id dari database (gunakan fungsi GET dan mysqli_fetch_assoc() 
             // serta query SELECT dan WHERE)
-            
+            $query = mysqli_query($connect, "SELECT * FROM showroom_mobil WHERE id='$id'");
+            $selects = mysqli_fetch_assoc($query);
 
 
 
